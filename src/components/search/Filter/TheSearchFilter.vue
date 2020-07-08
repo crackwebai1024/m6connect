d79f7d53c06b2687ef2f4562c1a53369ad601f79
Hamza <hamzabitup@gmail.com>
2020-07-08 15:25:59 +0100

    Add: layout dropdown to the search filter

--- src/components/search/Filter/TheSearchFilter.vue
+++ src/components/search/Filter/TheSearchFilter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="search-filter">
     <v-container fluid>
-      <v-row justify="space-between">
+      <v-row justify="space-between px-2">
         <v-col>
           <h1 class="h1">
             M6Companies
@@ -12,6 +12,7 @@
         </v-col>
         <v-col class="align-center d-flex justify-end">
           <search-dropdown />
+          <layout-dropdown />
         </v-col>
       </v-row>
     </v-container>
@@ -22,11 +23,13 @@
 import { Component, Vue } from 'vue-property-decorator'
 import TheToolbar from '@toolbar/TheToolbar.vue'
 import SearchDropdown from './SearchDropdown.vue'
+import LayoutDropdown from './LayoutDropdown.vue'
 
 @Component({
   components: {
     TheToolbar,
-    SearchDropdown
+    SearchDropdown,
+    LayoutDropdown
   }
 })
 export default class TheSearchFilter extends Vue {}
