cb8233367fff658bb74e4ba74e8730d081cdc8d5
Jose Manuel <manuelvaldez8642@gmail.com>
2020-08-19 13:46:51 -0500

    Home page
    Structure of home

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>Home</div>
+  <v-row no-gutters>
+    <v-col cols="3">
+      <v-card
+        class="pa-2"
+        outlined
+        tile
+      >
+        Proyect Info
+      </v-card>
+    </v-col>
+    <v-col cols="7">
+      <v-card
+        class="pa-2"
+        outlined
+        tile
+      >
+        Proyects List
+      </v-card>
+    </v-col>
+    <v-col cols="2">
+      <v-card
+        class="pa-2"
+        outlined
+        tile
+      >
+        Team List
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
