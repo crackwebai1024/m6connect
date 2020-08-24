ec62fed611108020b807ca6bcb0f0a4fff98c027
Jose Manuel <manuelvaldez8642@gmail.com>
2020-08-24 13:56:09 -0500

    feat: Post component V1
    In that commit is added the first view at the post

---
+++ src/components/Home/SocialMedia/PostItem.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-card>
+        <v-card-title>
+            <v-row>
+                <v-col cols="2">
+                    <v-avatar>
+                        <img :src=data.imageUrl>
+                    </v-avatar>
+                </v-col>
+                <v-col cols="8">
+                    <h2 class="text-md-body-1 font-weight-bold">{{data.name}}</h2>
+                    <p class="text-caption"> {{data.occupation}} </p>
+                </v-col>
+                <v-col cols="2">
+                    <v-icon>mdi-close</v-icon>
+                </v-col>
+            </v-row>
+        </v-card-title>
+        <v-card-text>
+            <v-row no-gutters>
+                <v-col :cols="widthCols()" v-for="(image, index) of data.images" :key="index">
+                    <v-img contain :src=image.url class="rounded-lg mx-1 my-1" aspect-ratio="1.7"></v-img>
+                </v-col>
+            </v-row>
+            <v-row>
+                <p>{{data.contain}}</p>
+            </v-row>
+        </v-card-text>
+        <v-card-actions>
+            <v-btn @click="print">
+                asdasd
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+<script>
+export default {
+    name:"PostItem",
+    props: {
+        data: Object
+    },
+    methods: {
+        widthCols(){
+            return this.data.images.length === 1 ? 12 : 6;
+        },
+        print(){
+            console.log(this.widthCols());
+        }
+    }
+}
+</script>
---
+++ src/components/Home/SocialMedia/PostsList.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container class="px-0 py-0">
+        <post-item :data="item"/>
+    </v-container>
+</template>
+<script>
+import PostItem from "./PostItem";
+
+export default {
+    name: "PostsList",
+    components: {
+        PostItem
+    },
+    data: () => ({
+        item:{
+            contain: "Ad do incididunt reprehenderit enim est laborum. Enim dolore do est eu incididunt. Commodo commodo occaecat tempor magna anim. Minim aute nisi culpa sint est et officia elit. Eiusmod eiusmod cupidatat qui qui mollit et irure nulla eiusmod reprehenderit culpa dolor laboris. Consequat pariatur cillum anim minim.",
+            name: "Jose Manuel Valdez Gonzalez",
+            imageUrl: "https://cdn.vuetifyjs.com/images/john.jpg",
+            occupation: "IT Apps (Department)",
+            images:[
+                {url:"https://www.pngitem.com/pimgs/m/160-1600311_free-png-download-happy-person-png-images-background.png"},
+                {url:"https://picsum.photos/1280/720?nocache=" + Math.random()},
+                {url:"https://picsum.photos/1280/720?nocache=" + Math.random()},
+                {url:"https://picsum.photos/1280/720?nocache=" + Math.random()},
+            ]
+        }
+    })
+}
+</script>
