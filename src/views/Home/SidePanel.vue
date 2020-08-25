c28c24de031d4e886205119eb22887175dec566b
Fiorella Effio Saldivar <fiorella.effio@ivisa.com>
2020-08-25 15:46:30 -0500

    [NEW] iterate array

--- src/views/Home/SidePanel.vue
+++ src/views/Home/SidePanel.vue
@@ -2,13 +2,7 @@
   <div>
     <div class="blue darken-3 ma-2 py-3 px-4">
       <div class="d-flex">
-        <img
-          alt=""
-          class="mr-2"
-          width="100"
-          height="65"
-          src="@/assets/sharp-logo.png"
-        >
+        <img alt="" class="mr-2" width="100" height="65" src="@/assets/sharp-logo.png">
         <h2 class="ml-2 white--text spacing-tight font-weight-medium">Sharp Healthcare</h2>
       </div>
       <v-divider class="mb-3 mt-4 light-blue accent-1"></v-divider>
@@ -33,32 +27,21 @@
       </div>
     </div>
     <div class="mt-3 t-6 white ma-2 pt-4 pb-5 px-4">
-      <p class="ml-3 grey--text text--lighten-1 body-2  mb-3">Links</p>
-      <div class="d-flex">
-        <img
-          alt=""
-          width="150"
-          height="75"
-          src="@/assets/image.png"
-        >
-        <div class="ml-3">
-          <p class="mb-1 font-weight-medium leading-tight spacing-tight">Cool title</p>
-          <p class="body-2 mb-0">Lorem ipsum dolor sit amet, consetetus sadipscing elitr, sed.</p>
+      <p class="ml-3 grey--text text--lighten-1 body-2 mb-3">Links</p>
+      <template v-for="(card, index) in cardsList">
+        <div :key="'card' + index">
+          <div class="d-flex">
+            <img alt="" width="150" height="70" src="@/assets/image.png">
+            <div class="ml-3">
+              <p class="mb-1 font-weight-medium leading-tight spacing-tight">{{ card.title }}</p>
+              <p class="body-2 mb-0 leading-tight">{{ card.description }}</p>
+            </div>
+          </div>
+          <div v-if="index !== cardsList.length - 1">
+            <v-divider class="my-3 grey lighten-2"></v-divider>
+          </div>
         </div>
-      </div>
-      <v-divider class="my-3 grey lighten-2"></v-divider>
-      <div class="d-flex">
-        <img
-          alt=""
-          width="150"
-          height="75"
-          src="@/assets/image.png"
-        >
-        <div class="ml-3">
-          <p class="mb-1 font-weight-medium leading-tight spacing-tight">Cool title</p>
-          <p class="body-2 mb-0 leading-tight">Lorem ipsum dolor sit amet, consetetus sadipscing elitr, sed.</p>
-        </div>
-      </div>
+      </template>
     </div>
   </div>
 </template>
@@ -77,7 +60,12 @@ export default {
       { url:'', icon:'shield-half-full', title: 'Capital Projects' },
       { url:'', icon:'earth', title: 'Request for Proposal' },
       { url:'', icon:'alert', title: 'Contracts' },
+      { url:'', icon:'asterisk', title: 'Forensics' },
       { url:'', icon:'biohazard', title: 'BioMed' }
+    ],
+    cardsList: [
+      { imgSrc:'', title: 'Cool Title', description: 'Lorem ipsum dolor sit amet, consetetus sadipscing elitr, sed.'},
+      { imgSrc:'', title: 'Cool Title', description: 'Lorem ipsum dolor sit amet, consetetus sadipscing elitr, sed.' }
     ]
   }),
   name: "SidePanel",
