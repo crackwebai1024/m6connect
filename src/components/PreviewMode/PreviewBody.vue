47fbd3120627e4c636122862f49765486758745c
Jose Manuel <manuelvaldez8642@gmail.com>
2020-08-26 23:53:42 -0500

    [CHORE] Install cue-infinite-loading

--- src/components/PreviewMode/PreviewBody.vue
+++ src/components/PreviewMode/PreviewBody.vue
@@ -1,13 +1,20 @@
 <template>
-    <v-container class="vertical-scroll dont-show-scroll" style="height:70vh">
+    <v-container class="vertical-scroll" style="height:70vh">
         <!-- That ID is used to scrolling the component -->
-        <component :id="getName()+'-'+index" v-bind:is="item" v-for="(item, index) of NavCommp" :key="index"></component>
+        <component :id="getName()+'-'+index" v-bind:is="item" v-for="(item, index) of items" :key="index"></component>
+        <infinite-loading @infinite="infiniteHandler">
+            <div slot="no-more"></div>
+        </infinite-loading>
     </v-container>
 </template>
 <script>
 
 export default {
     name: "PreviewBody",
+    data: () => ({
+        items:[],
+        currentIndex:0
+    }),
     props: {
         NavCommp: Array,
         name: String
@@ -15,6 +22,22 @@ export default {
         getName(){
             let name = this.name.split(' ');
             return name.join('-')
+        },
+        infiniteHandler($state) {
+            if(this.currentIndex < this.NavCommp.length){
+                this.items.push(this.NavCommp[this.currentIndex])
+                this.currentIndex++;
+                $state.loaded();
+            }else{
+                $state.complete();
+                $state.resete();
+            this.items = [];
+            this.currentIndex = 0;
+            }
+        },
+    },
+    watch:{
+        NavCommp:function(){
         }
     }
 }
