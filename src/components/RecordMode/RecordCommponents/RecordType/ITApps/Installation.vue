738c950b69c387743601178fd83b8b8986702803
Kelly <67666284+KellySugarcoat@users.noreply.github.com>
2020-09-01 16:50:06 -0300

    [MOD] (ITApps) Installation component

--- src/components/RecordMode/RecordCommponents/RecordType/ITApps/Installation.vue
+++ src/components/RecordMode/RecordCommponents/RecordType/ITApps/Installation.vue
@@ -3,7 +3,7 @@
       <v-subheader class="primary rounded-t white--text">Installation</v-subheader>
       <v-expansion-panels accordion>
           <v-expansion-panel>
-              <v-expansion-panel-header>General</v-expansion-panel-header>
+              <v-expansion-panel-header class="font-weight-medium">General</v-expansion-panel-header>
               <v-expansion-panel-content>
                   <!--Vamos a hacer el item más compacto en caso de que su value esté vacio-->
                     <v-list-item :two-line="item.value.length > 0" :key="'general-' + key"
@@ -17,23 +17,37 @@
           </v-expansion-panel>
           
           <v-expansion-panel>
-              <v-expansion-panel-header>Attachments</v-expansion-panel-header>
+              <v-expansion-panel-header class="font-weight-medium">Attachments</v-expansion-panel-header>
               <v-expansion-panel-content>
-                  contenido aki
+                  <v-data-table>
+
+                  </v-data-table>
               </v-expansion-panel-content>
           </v-expansion-panel>
           
           <v-expansion-panel>
-              <v-expansion-panel-header>Install Support</v-expansion-panel-header>
+              <v-expansion-panel-header class="font-weight-medium">Install Support</v-expansion-panel-header>
               <v-expansion-panel-content>
-                  contenido aki
+                    <v-list-item :two-line="item.value.length > 0" :key="'support-' + key"
+                        v-for="(item, key) in installation.support">
+                        <v-list-item-content>
+                            <v-list-item-title class="text-body-2">{{item.label}}</v-list-item-title>
+                            <v-list-item-subtitle>{{item.value}}</v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
               </v-expansion-panel-content>
           </v-expansion-panel>
           
           <v-expansion-panel>
-              <v-expansion-panel-header>Additional Package Build Information</v-expansion-panel-header>
+              <v-expansion-panel-header class="font-weight-medium">Additional Package Build Information</v-expansion-panel-header>
               <v-expansion-panel-content>
-                  contenido aki
+                  <v-list-item :two-line="item.value.length > 0" :key="'support-' + key"
+                        v-for="(item, key) in installation.build_info">
+                        <v-list-item-content>
+                            <v-list-item-title class="text-body-2">{{item.label}}</v-list-item-title>
+                            <v-list-item-subtitle>{{item.value}}</v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
               </v-expansion-panel-content>
           </v-expansion-panel>
       </v-expansion-panels>
@@ -81,6 +95,61 @@ export default {
                     label: "General Notes",
                     value: ""
                 },
+            },
+            support: {
+                firewall_exceptions: {
+                    label: "Firewall Exceptions",
+                    value: ""
+                },
+                antivirus_exclusion: {
+                    label: "Firewall Exceptions",
+                    value: ""
+                },
+                mapped_drives: {
+                    label: "Mapped Drives",
+                    value: ""
+                },
+                shortcut_modifications: {
+                    label: "Shortcut Modifications",
+                    value: ""
+                },
+                ini_changes: {
+                    label: "INI Changes",
+                    value: ""
+                },
+                install_notes: {
+                    label: "Install Notes",
+                    value: ""
+                },
+                registry_changes: {
+                    label: "Registry Changes",
+                    value: ""
+                }
+            },
+            build_info: {
+                previous_version: {
+                    label: "Previous Software Version",
+                    value: ""
+                },
+                ad_machine: {
+                    label: "AD Groups - Machine",
+                    value: ""
+                },
+                ad_user: {
+                    label: "AD Groups - User",
+                    value: ""
+                }
+            },
+            attachments: {
+                items: [],
+                headers: [
+                    {text: "File name"},
+                    {text: "Attachment"},
+                    {text: "Revision notes"},
+                    {text: "Created at"},
+                    {text: "Submitter"},
+                    {text: "Action"},
+                ]
             }
         }
     }),
