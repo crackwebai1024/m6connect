7fa96ba8b4d6b4f1cac3aaecfa3d443f2f86b041
Kelly <67666284+KellySugarcoat@users.noreply.github.com>
2020-09-02 12:33:17 -0300

    [New] (ITApps) Rationalization Users component

--- src/components/RecordMode/RecordCommponents/RecordType/ITApps/Rationalization/Users.vue
+++ src/components/RecordMode/RecordCommponents/RecordType/ITApps/Rationalization/Users.vue
@@ -1,12 +1,58 @@
 <template>
   <v-container>
-    <v-subheader class="rounded-t primary white--text">Rationalization Users</v-subheader>
+    <v-dialog v-model="createModal" persistent max-width="600px">
+      <v-card>
+        <v-card-title class="blue darken-3 white--text">
+          <span class="headline">Create Governance</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-select label="User Type" :items="['Estimated Concurrent Users', 'Estimated Users']"></v-select>
+            </v-row>
+            <v-row>
+              <v-select label="Users"></v-select>
+            </v-row>
+            <v-row>
+              <v-textarea label="Notes"></v-textarea>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text @click="createModal = false">Close</v-btn>
+          <v-btn color="primary" @click="createModal = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-subheader style="position:relative" class="rounded-t primary white--text">
+      Rationalization Users
+      <v-btn
+        color="indigo"
+        dark
+        small
+        absolute
+        bottom
+        right
+        fab
+        @click="createModal = true">
+          <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </v-subheader>
+    <v-card>
+      <v-data-table>
+
+      </v-data-table>
+    </v-card>
   </v-container>
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    createModal: false
+  })
 }
 </script>
 
