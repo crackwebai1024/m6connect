e1dd1508b027058b9d476d4713a38d1cf9ac77f1
Fiorella Effio Saldivar <fiorella.effio@ivisa.com>
2020-09-12 11:54:19 -0500

    [FIX] Costs Panel working

--- src/components/RecordMode/RecordComponents/RecordType/ITApps/Rationalization/Costs.vue
+++ src/components/RecordMode/RecordComponents/RecordType/ITApps/Rationalization/Costs.vue
@@ -1,39 +1,68 @@
 <template>
   <v-container @mouseenter="isHover = true" @mouseleave="isHover = false">
-    <v-dialog v-model="createModal" persistent max-width="600px">
-      <v-card>
-        <v-card-title class="blue darken-3 white--text">
-          <span class="headline">Create Costs</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-select label="Cost Category" :items="options.cost_category"></v-select>
-            </v-row>
-            <v-row>
-              <v-select label="Cost Type" :items="options.cost_type"></v-select>
-            </v-row>
-            <v-row>
-              <v-select label="Cost Owner" :items="options.cost_owner"></v-select>
-            </v-row>
-            <v-row>
-              <v-select label="Period" :items="options.period"></v-select>
-            </v-row>
-            <v-row>
-              <v-input persistent-hint hint="$ 0.00">Cost</v-input> 
-            </v-row>
-            <v-row>
-              <v-textarea label="Notes"></v-textarea>
-            </v-row>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn text @click="createModal = false">Close</v-btn>
-          <v-btn color="primary" @click="createModal = false">Save</v-btn>
-        </v-card-actions>
-      </v-card>
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <v-form
+        ref="form"
+        v-model="valid"
+        class="white"
+      >
+        <v-card>
+          <v-card-title class="blue darken-3 white--text d-flex justify-space-between">
+            <span class="headline">{{ titleDialog }}</span>
+            <v-btn icon color="white" @click="deleteItem" v-if="!dialogMode">
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-select 
+                  label="Cost Category" 
+                  :items="options.cost_category"
+                  v-model="itemInfo.costCategory"
+                  :rules="selectRules"
+                ></v-select>
+              </v-row>
+              <v-row>
+                <v-select 
+                  label="Cost Type" 
+                  :items="options.cost_type"
+                  v-model="itemInfo.costType"
+                  :rules="selectRules"
+                ></v-select>
+              </v-row>
+              <v-row>
+                <v-select 
+                  label="Cost Owner" 
+                  :items="options.cost_owner"
+                  v-model="itemInfo.costOwner"
+                  :rules="selectRules"
+                ></v-select>
+              </v-row>
+              <v-row>
+                <v-select 
+                  label="Period" 
+                  :items="options.period"
+                  v-model="itemInfo.period"
+                  :rules="selectRules"
+                ></v-select>
+              </v-row>
+              <v-row>
+                <v-input persistent-hint readonly :hint="'$ ' +  itemInfo.cost.toString()">Cost</v-input> 
+              </v-row>
+              <v-row>
+                <v-textarea v-model="itemInfo.notes" label="Notes"></v-textarea>
+              </v-row>
+            </v-container>
+            <small>*indicates required field</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text @click="closeDialog">Close</v-btn>
+            <v-btn color="primary" :disabled="!valid" @click="clickAction">{{ titleAction }}</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-form>
     </v-dialog>
     <v-subheader style="position:relative" class="rounded-t primary white--text">
       Rationalization Costs
@@ -45,42 +74,79 @@
         bottom
         right
         fab
-        @click="createModal = true">
+        @click="dialog = true, dialogMode = true">
           <v-icon>mdi-plus</v-icon>
       </v-btn>
     </v-subheader>
     <v-card class="pt-5">
-      <v-data-table :headers="table.headers">
-
+      <v-data-table
+        :headers="table.headers"
+        :items="items"
+        item-key="name"
+        class="elevation-0"
+      >
+        <template v-slot:item.actions="{ item }">
+          <v-icon
+            small
+            class="mr-2"
+            color="blue lighten-1"
+            @click="showUpdateDialog(item)"
+          >
+            mdi-pencil
+          </v-icon>
+        </template>
       </v-data-table>
     </v-card>
   </v-container>
 </template>
 
 <script>
+import {items} from "@/mixins/items"
+import {validations} from "@/mixins/form-validations"
+
 export default {
+  name: "Costs",
+  mixins: [items, validations],  
   data: () => ({
-    createModal: false,
     isHover: false,
+    itemsName: 'Costs',
+    itemInfo: {
+      costCategory: null,
+      costType: null,
+      costOwner: null,
+      period: null,
+      cost: 0,
+      notes: null
+    },
     table: {
       headers: [
         {
-          text: 'Cost Type'
+          text: 'Cost Category',
+          value: 'costCategory'
         },
         {
-          text: 'Cost Owner'
+          text: 'Cost Type',
+          value: 'costType'
         },
         {
-          text: 'Cost'
+          text: 'Cost Owner',
+          value: 'costOwner'
         },
         {
-          text: 'Period'
+          text: 'Period',
+          value: 'period'
         },
         {
-          text: 'Notes'
+          text: 'Cost',
+          value: 'cost'
         },
         {
-          text: 'Actions'
+          text: 'Notes',
+          value: 'notes'
+        },
+        {
+          text: 'Actions',
+          value: 'actions'
         },
       ]
     },
