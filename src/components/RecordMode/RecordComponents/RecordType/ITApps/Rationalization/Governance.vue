b11906b1e01e0350807976530678026a4eaf9d7c
Fiorella Effio Saldivar <fiorella.effio@ivisa.com>
2020-09-12 07:09:00 -0500

    [FIX] Governance panel

--- src/components/RecordMode/RecordComponents/RecordType/ITApps/Rationalization/Governance.vue
+++ src/components/RecordMode/RecordComponents/RecordType/ITApps/Rationalization/Governance.vue
@@ -1,42 +1,84 @@
 <template>
   <v-container @mouseenter="isHover = true" @mouseleave="isHover = false">
-    <v-dialog v-model="createModal" persistent max-width="600px">
-      <v-card>
-        <v-card-title class="blue darken-3 white--text">
-          <span class="headline">Create Governance</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-select label="Responsible Division" :items="options.resp_division"></v-select>
-            </v-row>
-            <v-row>
-              <v-select label="Responsible Comitee" :items="options.resp_comittee"></v-select>
-            </v-row>
-            <v-row>
-              <v-select label="Responsible Manager" :items="options.resp_manager"></v-select>
-            </v-row>
-            <v-row>
-              <v-select label="Responsible VP/Dir" :items="options.resp_dir"></v-select>
-            </v-row>
-            <v-row>
-              <v-select label="First Contact Group" :items="options.contact_group"></v-select>
-            </v-row>
-            <v-row>
-              <v-text-field label="Primary Customer Group"></v-text-field>
-            </v-row>
-            <v-row>
-              <v-text-field label="Estimated Days to Replace"></v-text-field>
-            </v-row>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn text @click="createModal = false">Close</v-btn>
-          <v-btn color="primary" @click="createModal = false">Save</v-btn>
-        </v-card-actions>
-      </v-card>
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <v-form
+        ref="form"
+        v-model="valid"
+        class="white"
+      >
+        <v-card>
+          <v-card-title class="blue darken-3 white--text d-flex justify-space-between">
+            <span class="headline">Create Governance</span>
+            <v-btn icon color="white" @click="deleteItem" v-if="!dialogMode">
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-select 
+                  v-model="itemInfo.respDivision" 
+                  :rules="selectRules" 
+                  label="Responsible Division" 
+                  :items="options.resp_division"
+                ></v-select>
+              </v-row>
+              <v-row>
+                <v-select 
+                  v-model="itemInfo.respComitte" 
+                  :rules="selectRules" label="Responsible Comitee" 
+                  :items="options.resp_comittee"
+                ></v-select>
+              </v-row>
+              <v-row>
+                <v-select 
+                  v-model="itemInfo.respManager" 
+                  :rules="selectRules" label="Responsible Manager" 
+                  :items="options.resp_manager"
+                ></v-select>
+              </v-row>
+              <v-row>
+                <v-select 
+                  v-model="itemInfo.respVpDir" 
+                  :rules="selectRules" label="Responsible VP/Dir" 
+                  :items="options.resp_dir"
+                ></v-select>
+              </v-row>
+              <v-row>
+                <v-select 
+                  v-model="itemInfo.firstContactGroup" 
+                  :rules="selectRules" label="First Contact Group" 
+                  :items="options.contact_group"
+                ></v-select>
+              </v-row>
+              <v-row>
+                <v-text-field 
+                  v-model="itemInfo.primaryCustomerGroup" 
+                  :rules="textRules" label="Primary Customer Group"
+                ></v-text-field>
+              </v-row>
+              <v-row>
+                <v-text-field 
+                  v-model="itemInfo.daysReplace" 
+                  :rules="textRules" 
+                  label="Estimated Days to Replace"
+                ></v-text-field>
+              </v-row>
+            </v-container>
+            <small>*indicates required field</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text @click="closeDialog">Close</v-btn>
+            <v-btn
+              :disabled="!valid"
+              text
+              color="primary" 
+              @click="clickAction"
+            >{{ titleAction }}</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-form>
     </v-dialog>
     <v-subheader style="position:relative" class="rounded-t primary white--text">
       Rationalization Governance
@@ -48,55 +90,85 @@
         bottom
         right
         fab
-        @click="createModal = true">
+        @click="dialog = true, dialogMode = true">
           <v-icon>mdi-plus</v-icon>
       </v-btn>
     </v-subheader>
     
     <v-card class="pt-5">
-      <v-data-table :headers="table.headers"></v-data-table>
+      <v-data-table 
+        :headers="table.headers"
+        :items="items"
+        :single-select="singleSelect"
+        item-key="name"
+        class="elevation-0"
+      >
+        <template v-slot:item.actions="{ item }">
+          <v-icon
+            small
+            class="mr-2"
+            color="blue lighten-1"
+            @click="showUpdateDialog(item)"
+          >
+            mdi-pencil
+          </v-icon>
+        </template>
+      </v-data-table>
     </v-card>
   </v-container>
 </template>
 
 <script>
+import {items} from "@/mixins/items";
+import {validations} from "@/mixins/form-validations"
+
 export default {
+  mixins: [items, validations],
   data: () => ({
     isHover: false,
     createModal: false,
+    itemInfo: {
+      respDivision: null,
+      respComitte: null,
+      respManager: null,
+      respVpDir: null,
+      firstContactGroup: null,
+      primaryCustomerGroup: null,
+      daysReplace: null,
+    },
     table: {
       headers: [
         {
           text: 'Responsible Division',
-          value: 'name'
+          value: 'respDivision'
         },
         {
           text: 'Responsible Comittee',
-          value: 'a'
+          value: 'respComitte'
         },
         {
           text: 'Responsible Manager',
-          value: 'b'
+          value: 'respManager'
         },
         {
           text: 'Responsible VP/Dir',
-          value: 'eee'
+          value: 'respVpDir'
         },
         {
           text: 'First Contact Group',
-          value: 'dddd'
+          value: 'firstContactGroup'
         },
         {
           text: 'Primary Customer Group',
-          value: 'ss'
+          value: 'primaryCustomerGroup'
         },
         {
           text: 'Estimated days to Replace',
-          value: 'aa'
+          value: 'daysReplace'
         },
         {
           text: 'Actions',
-          value: 'namebb'
+          value: 'actions'
         },
       ]
     },
