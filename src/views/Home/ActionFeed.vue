788361670788c943d12d742fa976c61ee0e60d2c
Fiorella Effio Saldivar <fiorella.effio@ivisa.com>
2020-09-16 23:46:44 -0500

    Action Feed UI

--- src/views/Home/ActionFeed.vue
+++ src/views/Home/ActionFeed.vue
@@ -1,48 +1,59 @@
 <template>
-  <div class="company-menu transparent">
-    <div class="blue darken-3 mt-3 ml-2 py-3 px-4">
+  <div class="actionfeed-content transparent ml-4">
+    <div class="pl-3 mt-4 mb-3 d-flex justify-space-between align-center">
+      <p class="font-weight-bold mb-0">Action Feed</p>
+      <div class="d-flex align-center">
+        <p class="mb-0 purple--text text--darken-1 font-weight-bold">
+          All Apps 
+          <v-icon style="margin-left: -6px;" class="blue--text text--darken-3">mdi-chevron-down</v-icon>
+        </p>
+        <v-icon size="28" class="ml-2">mdi-magnify</v-icon>
+      </div>
+    </div>
+    <div v-for="post in 4" :key="post" class="actionfeed-content__card rounded white mb-4 pt-4 px-3">
+      <div class="card-content__tag red white--text d-flex justify-center align-center text-body-1 font-weight-regular">
+        CPM
+      </div>
       <div class="d-flex">
-        <img alt="" class="mr-2" width="100" height="65" src="@/assets/sharp-logo.png">
-        <h2 class="ml-2 white--text spacing-tight font-weight-medium">Sharp Healthcare</h2>
-      </div>
-      <v-divider class="mb-3 mt-4 light-blue accent-1"></v-divider>
-      <div>
-        <p class="ml-3 grey--text text--lighten-2 body-2 mb-1">Quick Access</p>
-        <div class="d-flex align-center my-3" v-for="(link,i) in quickAccessLinks" :key="'link' + i">
-          <v-icon :dark="true" :large="true">mdi-{{ link.icon }}</v-icon>
-          <router-link :to="link.url" tag="p" class="ml-5 w-4/5 white--text mb-0">{{ link.title }}</router-link>
-        </div>
-        <v-divider class="my-3 light-blue accent-1"></v-divider>
-        <p class="ml-3 grey--text text--lighten-2 body-2 mb-1">Departments</p>
-        <div class="overflow-hidden links" ref="showLinksDiv">
-          <div class="d-flex align-center my-3" v-for="(link,i) in departmentsLinks" :key="'link2' + i">
-            <v-icon :dark="true" :large="true">mdi-{{ link.icon }}</v-icon>
-            <p class="ml-5 w-4/5 white--text mb-0">{{ link.title }}</p>
-          </div>
-        </div>
-        <div @click="toogleLinks" class="cursor-pointer d-flex align-center pt-2">
-          <v-icon :dark="true" :large="true">mdi-chevron-{{ iconShowLinks }}</v-icon>
-          <p class="ml-5 white--text mb-0">{{ showLinksMessage }}</p>
+        <v-avatar size="36">
+          <v-img src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"></v-img>
+        </v-avatar>
+        <div class="ml-2">
+          <p class="font-weight-bold" style="margin-bottom: -5px;">Username M. Johnson</p>
+          <span class="text-caption grey--text text--darken-1">Project Manager - 15 min</span>
         </div>
       </div>
+      <p class="text-body-2 mt-2 mb-3 ml-2 grey--text text--darken-4">This is the budget for next year. Please review.</p>
+      <p class="text-body-2 ml-1 mb-0 blue--text text--darken-3 d-flex align-center">
+        <v-icon class="mr-1 blue--text text--darken-3">mdi-file-document-outline</v-icon>
+        Elevator Modernization All Hospitals.cpm.bdg
+      </p>
+      <div class="d-flex mt-4 ml-2 align-center">
+        <v-badge
+          v-for="i in 3" :key="i" style="margin-left:-5px"
+          top
+          color="green lighten-2"
+          dot
+          offset-x="9"
+          offset-y="7"
+        >
+          <v-avatar size="28">
+            <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
+          </v-avatar>
+        </v-badge>
+        <p class="ml-2 mb-0 text-caption grey--text text--darken-1">2 pending</p>
+      </div>
+      <div class="d-flex feed-btns pa-3 text-caption align-center">
+        <p class="mb-0 mr-2"><v-icon size="17">mdi-thumb-up-outline</v-icon> 157</p>
+        <p class="mb-0 mr-2"><v-icon size="17">mdi-message-outline</v-icon> 14</p>
+        <p class="mb-0 mr-2"><v-icon size="17">mdi-share</v-icon> 4</p>
+      </div>
     </div>
-    <div class="mt-3 t-6 white ml-2 pt-4 pb-5 px-4">
-      <p class="ml-3 grey--text text--lighten-1 body-2 mb-3">Links</p>
-      <template v-for="(card, index) in cardsList">
-        <div :key="'card' + index">
-          <div class="d-flex">
-            <img alt="" width="150" height="70" src="@/assets/image.png">
-            <div class="ml-3">
-              <p class="mb-1 font-weight-medium leading-tight spacing-tight">{{ card.title }}</p>
-              <p class="body-2 mb-0 leading-tight">{{ card.description }}</p>
-            </div>
-          </div>
-          <div v-if="index !== cardsList.length - 1">
-            <v-divider class="my-3 grey lighten-2"></v-divider>
-          </div>
-        </div>
-      </template>
-    </div>
+
+    <!-- <div class="d-flex align-center my-3" v-for="(link,i) in quickAccessLinks" :key="'link' + i">
+      <v-icon :dark="true" :large="true">mdi-{{ link.icon }}</v-icon>
+      <router-link :to="link.url" tag="p" class="ml-5 w-4/5 white--text mb-0">{{ link.title }}</router-link>
+    </div> -->
   </div>
 </template>
 
@@ -90,7 +101,7 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   .leading-tight {
     line-height: 1.4;
   }
@@ -115,7 +126,29 @@ export default {
     -o-transition: height .5s ease;
     transition: height .5s ease;
   }
-  .company-menu {
-    width: 315px;
+  .actionfeed-content {
+    width: 400px;
   }
+  .actionfeed-content__card {
+    position: relative;
+    min-height: 223px;
+    box-shadow: 0px 3px 6px #00000028;
+  }
+  .card-content__tag {
+    position: absolute;
+    top: 0px;
+    right: 0;
+    width: 62px;
+    height: 34px;
+    border-radius: 0px 4px;
+  }
+  .feed-btns {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+  }
+  // esto es para que se vean mejor las letras pequenhas
+  // .text-caption {
+  //   letter-spacing: 0 !important;
+  // }
 </style>
