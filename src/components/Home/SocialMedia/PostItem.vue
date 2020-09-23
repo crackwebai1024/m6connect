043b4834a0f52acb46d248b2ab57200f8890b086
Fiorella Effio Saldivar <fiorella.effio@ivisa.com>
2020-09-23 16:58:15 -0500

    post type request

--- src/components/Home/SocialMedia/PostItem.vue
+++ src/components/Home/SocialMedia/PostItem.vue
@@ -1,7 +1,10 @@
 <template>
-  <v-container class="px-0 py-0">
+  <v-container class="px-0 py-0 relative">
     <div class="mb-3 card-custom-shadow white rounded">
-      <v-card-title class="pb-0">
+      <div v-if="data['postTagTitle']" :class="tagColor + ' card-content__tag absolute white--text d-flex justify-center align-center text-body-1 font-weight-regular'">
+        {{ data['postTagTitle'] }}
+      </div>
+      <v-card-title class="pb-0 pt-6">
         <v-row no-gutters align="center">
           <div class="d-flex align-center">
             <v-avatar size="40" class="mr-2">
@@ -63,6 +66,38 @@
             :class="data.images.length !== 0 ? 'mt-3' : ''"
             v-line-clamp="5"
           >{{data.contain}}</div>
+        </div>
+      </div>
+      <div v-if="data['postType'] === 'request'" class="ma-4 mt-0 border-1">
+        <div :class="'text-h6 py-2 px-3 font-weight-regular ' + this.tagColor + '--text'">{{ data['request'].title }}</div>
+        <v-divider></v-divider>
+        <!-- here will live the other component -->
+        <div class="d-flex justify-space-between pa-3 pt-0 align-center">
+          <div>
+            <div class="pt-6 pb-2 d-flex align-center">
+              <div v-for="(i, index) in 3" :key="i" class="d-flex">
+                <v-badge
+                  top
+                  color="green accent-3"
+                  icon="mdi-check"
+                  offset-x="9"
+                  offset-y="7"
+                >
+                  <v-avatar size="35">
+                    <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
+                  </v-avatar>
+                </v-badge>
+                <template v-if="index !== 3 - 1">
+                  <span style="height: 5px; width: 25px;" class="my-auto grey lighten-2"></span>
+                </template>
+              </div>
+            </div>
+            <div class="text-caption black--text">2 Pending Approvals from You and Hanna</div>
+          </div>
+          <div class="d-flex justify-end">
+            <v-btn class="my-2 mr-2 px-6 capitalize" text color="grey darken-3">Approve</v-btn>
+            <v-btn class="my-2 px-8 capitalize" outlined color="green accent-4">Approve</v-btn>
+          </div>
         </div>
       </div>
       <v-divider class="mx-4" />
@@ -192,15 +227,25 @@ export default {
   },
   computed: {
     ...mapGetters(["get_user_data"]),
+    tagColor() {
+      return this.data['postType'] === 'request' ? 'red' : 'teal accent-3';
+    }
   },
 };
 </script>
 
-<style scoped>
+<style>
 .repeating-gradient {
   background-color: rgba(38, 38, 38, 0.7);
 }
 /* .v-text-field.v-input--dense:not(.v-text-field--outlined) input {
   padding: 0 !important;
 } */
+.v-badge--icon .v-badge__badge {
+  height: 15px !important;
+  min-width: 15px !important;
+  display: flex;
+  justify-content: center;
+  padding: 0;
+}
 </style>
