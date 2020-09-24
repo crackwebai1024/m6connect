3b6e85bc4b43ba1010a2078729567f49f73ceea2
Fiorella Effio Saldivar <fiorella.effio@ivisa.com>
2020-09-23 20:17:41 -0500

    show action btns action feed

--- src/views/Home/ActionFeed.vue
+++ src/views/Home/ActionFeed.vue
@@ -21,58 +21,18 @@
         <v-icon size="28" class="ml-2">mdi-magnify</v-icon>
       </div>
     </div>
-    <div v-for="(notification, index) in notifications" :key="'notification-'+index" class="actionfeed-content__card relative card-custom-shadow rounded white mb-4 pt-4 px-3 pb-12">
-      <div :class="notification.colorTag +' card-content__tag absolute white--text d-flex justify-center align-center text-body-1 font-weight-regular'">
-        {{ notification.typeContent }}
-      </div>
-      <div class="d-flex">
-        <v-avatar size="36">
-          <v-img :src="notification.userFrom.imgSrc"></v-img>
-        </v-avatar>
-        <div class="ml-2">
-          <p class="font-weight-bold" style="margin-bottom: -5px;">{{ notification.userFrom.name }}</p>
-          <span class="text-caption grey--text text--darken-1">{{ notification.userFrom.title }} -  {{ diffNow(notification.date) }}</span>
-        </div>
-      </div>
-      <p class="text-body-2 mt-2 mb-3 ml-2 grey--text text--darken-4">{{ notification.textContent }}</p>
-      <p v-if="notification.notificationType === 'document'" :class="notification.colorTag + '--text ' + 'text-body-2 ml-1 mb-0 d-flex align-center'">
-        <v-icon :class="notification.colorTag + '--text ' + 'mr-1'">mdi-file-document-outline</v-icon>
-        {{ notification.message }}
-      </p>
-      <p v-if="notification.notificationType === 'message'" class="message-box text-caption black--text pl-3 ml-1 mb-0 d-flex align-center">
-        {{ notification.message }}
-      </p>
-      <div v-if="notification.notificationType !== 'message'" class="d-flex mt-4 ml-2 align-center">
-        <v-badge
-          v-for="(follower, index) in notification.followers" :key="index + 'follower'" style="margin-left:-5px"
-          :bordered="follower.review ? false : true"
-          :dark="follower.review ? false : true"
-          top
-          :color="follower.review ? 'green accent-3' : 'white black--text'"
-          :icon="follower.review ? 'mdi-check' : 'mdi-help'"
-          offset-x="12"
-          offset-y="12"
-        >
-          <v-avatar size="28">
-            <v-img :src="follower.imgSrc"></v-img>
-          </v-avatar>
-        </v-badge>
-        <p v-if="pendingApprovals(notification.followers) > 0" class="ml-2 mb-0 text-caption grey--text text--darken-1">
-          {{ pendingApprovals(notification.followers) }} pending
-        </p>
-      </div>
-      <div v-if="notification.notificationType !== 'message'" class="d-flex feed-btns absolute pa-3 text-caption align-center">
-        <p class="mb-0 mr-2"><v-icon size="17">mdi-thumb-up-outline</v-icon> {{ notification.likes }}</p>
-        <p class="mb-0 mr-2"><v-icon size="17">mdi-message-outline</v-icon> {{ notification.comments }}</p>
-        <p class="mb-0 mr-2"><v-icon size="17">mdi-share</v-icon> {{ notification.shared }}</p>
-      </div>
-    </div>
+    <action-feed-item v-for="(notification, index) in notifications" :key="'notification-'+index" :notification="notification"/>
   </div>
 </template>
 
 <script>
+import ActionFeedItem from './ActionFeedItem'
 export default {
+  components: {
+    ActionFeedItem,
+  },
   data: () => ({
+    showActionBtns: false,
     // action feed data 
     notifications: [
       {
@@ -197,20 +157,6 @@ export default {
         ],
       },
     ],
-    showLinks: false,
-    departmentsLinks: [
-      { url:'', icon:'chart-pie', title: 'Information Technology' },
-      { url:'', icon:'cookie', title: 'Supply Chain' },
-      { url:'', icon:'shield-half-full', title: 'Capital Projects' },
-      { url:'', icon:'earth', title: 'Request for Proposal' },
-      { url:'', icon:'alert', title: 'Contracts' },
-      { url:'', icon:'asterisk', title: 'Forensics' },
-      { url:'', icon:'biohazard', title: 'BioMed' }
-    ],
-    cardsList: [
-      { imgSrc:'', title: 'Cool Title', description: 'Lorem ipsum dolor sit amet, consetetus sadipscing elitr, sed.'},
-      { imgSrc:'', title: 'Cool Title', description: 'Lorem ipsum dolor sit amet, consetetus sadipscing elitr, sed.' }
-    ],
     areas: [
       { text: "Everyone", type: "subtitle" },
       { text: "My company", type: "subtitle" },
@@ -226,43 +172,8 @@ export default {
   }),
   name: "ActionFeed",
   computed: {
-    showLinksMessage: function() {
-      return this.showLinks ? 'Less' : 'More'
-    },
-    iconShowLinks: function()  {
-      return this.showLinks ? 'up' : 'down'
-    },
-    heightShowLinksDiv: function() {
-      return (this.departmentsLinks.length * 48) + 'px'
-    }
   },
   methods: {
-    toogleLinks() {
-      this.showLinks = !this.showLinks
-      let linksdiv = this.$refs.showLinksDiv
-      this.showLinks ? linksdiv.style.height = this.heightShowLinksDiv : linksdiv.style.height = "150px"
-    },
-    diffNow(date) {
-      let dateNow = new Date();
-      let dateNotification = new Date(date);
-      let diff =(dateNow.getTime() - dateNotification.getTime()) / 1000;
-      let seconds = Math.abs(Math.round(diff % 60));
-      diff = (diff - seconds) / 60;
-      let minutes = Math.abs(Math.round(diff % 60));
-      diff = (diff - minutes) / 60;
-      let hours = Math.abs(Math.round(diff % 24));
-      diff = (diff - hours) / 24;
-      let days = Math.abs(Math.round(diff % 30));
-      return days + ' days, ' + hours + ' hours, ' + minutes +' minutes, ' + seconds + ' seconds';
-    },
-    pendingApprovals(approvals) {
-      let pendingApprovals = 0 ;
-      approvals.forEach(element => {
-        if(!element.review) pendingApprovals++
-      });
-      
-      return pendingApprovals;
-    }
   }
 };
 </script>
@@ -285,6 +196,10 @@ export default {
   .feed-btns {
     bottom: 0;
     left: 0;
+  }
+  .action-btns {
+    bottom: 0;
+    right: 0;
   }
   .message-box {
     border-left: 2px solid grey;
