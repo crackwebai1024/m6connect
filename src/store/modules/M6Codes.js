8b38f1d11bcc143ba5cb7a027154a1ebeb7537af
cristian <segoviacristian23@hotmail.com>
2020-10-29 09:16:32 -0300

    NMFE-15 - removed 2 console.logs

--- src/store/modules/M6Codes.js
+++ src/store/modules/M6Codes.js
@@ -139,8 +139,6 @@ const actions = {
         return new Promise((resolve, reject) => {
             axios.post(`${process.env.VUE_APP_HTTP}${process.env.VUE_APP_ENDPOINT}/api/search/naicsCodes/ids`, { ids })
                 .then(res => {
-                    console.log('naics======res')
-                    console.log(res)
                     commit('multiSetNaics', res.data)
                     resolve(res.data)
                 })
