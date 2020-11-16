367e9b8d43b4af032114671de1ff1b99281f47e9
Hector Fierro <hafierrogarcia@gmail.com>
2020-11-16 13:41:41 -0700

    WIP

--- src/components/user/settings/SettingsSideNav.vue
+++ src/components/user/settings/SettingsSideNav.vue
@@ -1,49 +1,62 @@
 <template>
-    <v-card
-        class="mx-auto"
-        width="300"
-    >
-        <v-list>
-            <v-list-item >
-                <v-list-item-title @click="pickingComponent({ })" >My Profile</v-list-item-title>
-            </v-list-item>
+  <v-card
+    class="mx-auto"
+    width="300"
+  >
+    <v-list>
+      <v-list-item>
+        <v-list-item-title @click="pickingComponent({ })">
+          My Profile
+        </v-list-item-title>
+      </v-list-item>
 
-            <v-list-item class="cursor-pointer" >
-                <v-list-item-title @click="pickingComponent({ name: 'company-show', size: 9 })" >My Company</v-list-item-title>
-            </v-list-item>
+      <v-list-item class="cursor-pointer">
+        <v-list-item-title @click="pickingComponent({ name: 'company-show', size: 9 })">
+          My Company
+        </v-list-item-title>
+      </v-list-item>
 
-            <v-list-group >
-                <template v-slot:activator>
-                    <v-list-item-title >User Mangement</v-list-item-title>
-                </template>
+      <v-list-group>
+        <template v-slot:activator>
+          <v-list-item-title>User Mangement</v-list-item-title>
+        </template>
 
-                <v-list-item @click="pickingComponent({ name:'user-mangement-table' })" >All Users</v-list-item>
-            </v-list-group>
+        <v-list-item @click="pickingComponent({ name: 'user-mangement-table' })">
+          All Users
+        </v-list-item>
+      </v-list-group>
 
-            <div class="px-2" >
-                <hr />
-            </div>
+      <div class="px-2">
+        <hr>
+      </div>
 
-            <v-list-item >
-                <v-list-item-title >IT Applications</v-list-item-title>
-            </v-list-item>
+      <v-list-item>
+        <v-list-item-title>IT Applications</v-list-item-title>
+      </v-list-item>
 
-            <v-list-item >
-                <v-list-item-title >Capital Projects Management</v-list-item-title>
-            </v-list-item>
-        </v-list>
+      <v-list-item>
+        <v-list-item-title>
+          Capital Projects Management
+        </v-list-item-title>
+      </v-list-item>
+      <v-list-item @click="$router.push('/cpm/import/tool')">
+        <v-list-item-title>
+          Capital Projects Import Tool
+        </v-list-item-title>
+      </v-list-item>
+    </v-list>
   </v-card>
 </template>
 
 <script>
 import { mapMutations } from 'vuex'
 export default {
-   name: "SettingsSideNave" ,
-   methods: {
-       ...mapMutations('UserSettingsControl', {
-           pickingComponent : 'setSecondColumnComponent'
-       }),
-   }
+  name: 'SettingsSideNave',
+  methods: {
+    ...mapMutations('UserSettingsControl', {
+      pickingComponent: 'setSecondColumnComponent'
+    })
+  }
 }
 </script>
 
