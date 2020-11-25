d07b6a531a463721e9d168e944b5a42c9bddddd8
Hugo Escobar <hugo@ignisoft.click>
2020-11-24 20:42:05 -0700

    updates for app builder referenced_fields

--- src/components/AppBuilder/Modals/Field.vue
+++ src/components/AppBuilder/Modals/Field.vue
@@ -2,106 +2,170 @@
   <v-dialog
     v-model="show"
     persistent
-    width="500"
+    :scrollable="!loading"
+    :width="loading ? '400px' : '90%'"
   >
     <v-card
+      v-if="loading"
+      color="primary"
+      dark
+    >
+      <v-card-text>
+        Please stand by
+        <v-progress-linear
+          class="mb-0"
+          color="white"
+          indeterminate
+        />
+      </v-card-text>
+    </v-card>
+    <v-card
+      v-else
       class="elevation-0"
     >
-      <v-form
-        ref="fieldForm"
-        @submit.prevent
-      >
-        <v-card-text>
-          <v-text-field
-            v-model="field.label"
-            label="Field Name"
-          />
-          <v-autocomplete
-            v-model="field.type"
-            item-text="label"
-            item-value="value"
-            :items="types"
-            label="Field Type"
-          />
-          <v-text-field
-            v-model="field.machine_name"
-            label="Machine Name: Optional"
-          />
-
-          <v-divider class="my-2" />
-          <h3>
-            Options
-          </h3>
-          <template v-if="field.type === 'timestamp'">
-            <v-select
-              v-model="field.metadata.format"
-              :items="dateFormats"
-              label="Format"
-            />
-          </template>
-          <template v-if="field.type === 'autocomplete'">
-            <v-text-field
-              v-model="field.metadata.max"
-              label="Max Values"
-              value="1"
-            />
-            <v-text-field
-              v-model="field.metadata.min"
-              label="Min Values"
-              value="1"
-            />
-            <h4 class="mb-2">
-              Options
-            </h4>
-            <template v-for="(option, optIndex) in field.metadata.options">
-              <v-text-field
-                :key="optIndex"
-                v-model="field.metadata.options[optIndex]"
-                append-outer-icon="mdi-delete"
-                label="Option Name"
-                @click:append-outer="removeOption(optIndex, field.metadata.options)"
-              />
-            </template>
-            <v-btn
-              class="pa-0"
-              small
-              text
-              @click="pushOption"
-            >
-              Add Option
-            </v-btn>
-          </template>
-          <template v-if="field.type === 'reference'">
-            <treeselect
-              v-model="field.referenced_field"
-              :multiple="false"
-              :options="fieldList"
-              placeholder="Referenced Field"
-            />
-          </template>
-          <v-checkbox
-            v-model="field.metadata.required"
-            color="red"
-            hide-details
-            label="Required"
-            value="red"
-          />
-        </v-card-text>
-        <v-card-actions>
-          <v-btn @click="saveField(field)">
-            Save
-          </v-btn>
-          <v-btn @click="closeModal">
-            Cancel
-          </v-btn>
-        </v-card-actions>
-      </v-form>
+      <v-card-title>
+        <template v-if="editing">
+          Edit Field: {{ field.label }}
+        </template>
+        <template v-else>
+          Create New Field
+        </template>
+      </v-card-title>
+      <v-card-text class="mb-10">
+        <v-container>
+          <v-row>
+            <v-col cols="6">
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    v-model="field.label"
+                    label="Field Name"
+                  />
+                </v-col>
+                <v-col cols="12">
+                  <v-autocomplete
+                    v-model="field.type"
+                    item-text="label"
+                    item-value="value"
+                    :items="types"
+                    label="Field Type"
+                  />
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    v-model="field.machine_name"
+                    label="Machine Name: Optional"
+                  />
+                </v-col>
+                <v-col cols="12">
+                  <v-checkbox
+                    v-model="field.metadata.required"
+                    color="red"
+                    hide-details
+                    label="Required"
+                    value="red"
+                  />
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-col cols="6">
+              <v-row>
+                <v-col
+                  v-if="field.type === 'timestamp'"
+                  cols="12"
+                >
+                  <v-select
+                    v-model="field.metadata.format"
+                    :items="dateFormats"
+                    label="Format"
+                  />
+                </v-col>
+                <template v-if="field.type === 'autocomplete'">
+                  <v-col cols="12">
+                    <v-row>
+                      <v-col cols="6">
+                        <v-text-field
+                          v-model="field.metadata.min"
+                          label="Min Values"
+                          value="1"
+                        />
+                      </v-col>
+                      <v-col cols="6">
+                        <v-text-field
+                          v-model="field.metadata.max"
+                          label="Max Values"
+                          value="1"
+                        />
+                      </v-col>
+                    </v-row>
+                  </v-col>
+                  <v-col cols="12">
+                    <h4 class="mb-2">
+                      Options
+                    </h4>
+                    <template v-for="(option, optIndex) in field.metadata.options">
+                      <v-text-field
+                        :key="optIndex"
+                        v-model="field.metadata.options[optIndex]"
+                        append-outer-icon="mdi-delete"
+                        label="Option Name"
+                        @click:append-outer="removeOption(optIndex, field.metadata.options)"
+                      />
+                    </template>
+                    <v-btn
+                      class="pa-0"
+                      small
+                      text
+                      @click="pushOption"
+                    >
+                      Add Option
+                    </v-btn>
+                  </v-col>
+                </template>
+                <v-col
+                  v-if="field.type === 'referenced'"
+                  cols="12"
+                >
+                  <treeselect
+                    v-model="field.referenced_field"
+                    :disable-branch-nodes="true"
+                    :load-options="loadOptions"
+                    :multiple="false"
+                    :options="fieldList"
+                    placeholder="Referenced Field"
+                    value-format="object"
+                  />
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn
+          color="red"
+          text
+          @click="closeModal"
+        >
+          Cancel
+        </v-btn>
+        <v-btn
+          color="blue"
+          dark
+          @click="saveField(field)"
+        >
+          Save
+        </v-btn>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
 import { mapState, mapMutations } from 'vuex'
+import { LOAD_CHILDREN_OPTIONS } from '@riophae/vue-treeselect'
+
 import axios from 'axios'
 
 export default {
@@ -124,6 +188,7 @@ export default {
   },
   data() {
     return {
+      loading: true,
       dateFormats: [
         'mm/dd/YYYY',
         'mm/dd/YYYY H:m:s',
@@ -152,11 +217,20 @@ export default {
     // TODO: The available apps list should be on a global list on the store.
     axios.get(`${process.env.VUE_APP_HTTP}${process.env.VUE_APP_ENDPOINT}/api/dynamic_apps/apps`).then(response => {
       response.data.map(app => {
-        this.fieldList.push({
-          id: app.id,
-          label: app.title
-        })
+        if (parseInt(app.id) !== parseInt(this.$route.params.id)) {
+          this.fieldList.push({
+            id: app.title + app.id,
+            appId: app.id,
+            label: app.title,
+            children: null
+          })
+        }
       })
+      if (this.editing && this.field.type === 'referenced') {
+        // eslint-disable-next-line camelcase
+        this.field.referenced_field = this.field.metadata.originalReference
+      }
+      this.loading = false
     })
   },
 
@@ -171,13 +245,34 @@ export default {
     },
     async saveField(field) {
       try {
-        field.appID = this.currentApp.id
+        // eslint-disable-next-line camelcase
+        const postData = {
+          id: field.id,
+          // eslint-disable-next-line camelcase
+          app_id: this.currentApp.id,
+          // eslint-disable-next-line camelcase
+          panel_id: field.panel_id,
+          weight: field.weight,
+          metadata: field.metadata,
+          type: field.type,
+          label: field.label,
+          // eslint-disable-next-line camelcase
+          machine_name: field.machine_name
+        }
+        if (field.type === 'referenced') {
+          postData.metadata.originalReference = field.referenced_field
+          // eslint-disable-next-line camelcase
+          postData.referenced_field = field.referenced_field.id
+        } else {
+          delete postData.referenced_field
+          delete postData.metadata.originalReference
+        }
         if (this.editing) {
-          const data = await this.$store.dispatch('AppBuilder/updateField', field)
+          const data = await this.$store.dispatch('AppBuilder/updateField', postData)
           this.notifSuccess('The Field Was Updated')
           this.$emit('result', data)
         } else {
-          const data = await this.$store.dispatch('AppBuilder/saveField', field)
+          const data = await this.$store.dispatch('AppBuilder/saveField', postData)
           this.notifSuccess('The Field Was Created')
           this.$emit('result', data)
         }
@@ -199,10 +294,34 @@ export default {
       }
       this.field.metadata.options.push('New Option')
     },
-    normalizer(node) {
-      return {
-        label: node.value
+
+    loadOptions({ action, parentNode, callback }) {
+      if (action === LOAD_CHILDREN_OPTIONS) {
+        axios.post(`${process.env.VUE_APP_HTTP}${process.env.VUE_APP_ENDPOINT}/api/app-builder/field/list/all`, {
+          appId: parseInt(parentNode.appId),
+          nested: true
+        }).then(response => {
+          parentNode.children = this.normalizeNested(response.data)
+          callback()
+        })
       }
+    },
+
+    normalizeNested(tabs) {
+      return tabs.map(tab => {
+        tab.label = 'Tab: ' + tab.title
+        tab.id = tab.title + tab.id
+        tab.children = tab.panels.map(panel => {
+          panel.label = 'Panel: ' + panel.title
+          panel.id = panel.title + panel.id
+          panel.children = panel.fields.filter(field => field.type !== 'referenced').map(field => {
+            field.label = 'Field: ' + field.label
+            return field
+          })
+          return panel
+        })
+        return tab
+      })
     }
   }
 }
