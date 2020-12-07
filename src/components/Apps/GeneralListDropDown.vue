286015107ccec37628136e30de4fc12bbcfd4eb5
cristian <segoviacristian23@hotmail.com>
2020-12-07 16:13:50 -0300

    M6PLAT-15 - work in progress

---
+++ src/components/Apps/GeneralListDropDown.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-menu offset-y open-on-hover >
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        v-bind="attrs"
+        color="primary"
+        text 
+        v-on="on"
+      >
+        screen
+      </v-btn>
+    </template>
+    <v-list>
+      <v-list-item
+        v-for="(choice, index) in choices"
+        :key="index"
+      >
+        <v-list-item-title >
+          <v-btn text @click="$emit('input', choice.value)" >
+            {{ choice.text }} <v-icon class="black--text" >{{ choice.icon }}</v-icon>
+          </v-btn>
+        </v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+
+<script>
+export default {
+  name: "GeneralListDropDown",
+  data: () => ({
+    choices: [
+      { icon: 'mdi-table', text: 'Table View', value: true },
+      { icon: 'mdi-arrange-bring-forward', text: 'Card View', value: false }
+    ]
+  })
+}
+</script>
