32a32ba4a1696d046ea6cda2beedd7df89fabd53
james-jin-0903 <jinzhengyou0903@gmail.com>
2020-12-15 20:49:46 +0000

    Changed Tab to the Autocomplete ( Add new record dialog )

--- src/components/Dialogs/NewRecordDialog.vue
+++ src/components/Dialogs/NewRecordDialog.vue
@@ -1,84 +1,94 @@
 <template>
-    <v-card>
-        <v-card-title class="headline grey lighten-2">
-            Create Record
-        </v-card-title>
-        <v-card-text
-            class="record-text vertical-scroll">
-            <v-tabs 
-                centered
-                v-model="tab">
-                <v-tab v-for="item in items" :key="item.tab" >
-                    {{ item.tab }}
-                </v-tab>
-            </v-tabs>
-
-            <v-tabs-items v-model="tab">
-                <v-tab-item v-for="item in items" :key="item.tab" >
-                    <component 
-                        @closeModal="closeModal" 
-                        @closingGenericRecord="closingGenericRecord" 
-                        v-bind:is="item.component" 
-                        :app="item.app" 
-                    />
-                </v-tab-item> 
-            </v-tabs-items>
-        </v-card-text>
-    </v-card>
+  <v-card>
+    <v-card-title class="grey headline lighten-2">
+      Create Record
+    </v-card-title>
+    <v-card-text class="record-text vertical-scroll">
+      <v-autocomplete
+        v-model="tab"
+        auto-select-first
+        item-text="tab"
+        item-value="id"
+        :items="items"
+        label="Please select an APP"
+      />
+      <v-tabs-items v-model="tab">
+        <v-tab-item
+          v-for="item in items"
+          :key="item.tab"
+        >
+          <component
+            :is="item.component"
+            :app="item.app"
+            @closeModal="closeModal"
+            @closingGenericRecord="closingGenericRecord"
+          />
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
-import ItAppForm from "@/components/Home/Forms/ItAppForm";
+import ItAppForm from '@/components/Home/Forms/ItAppForm';
 import GenericRecord from '@/components/Home/Forms/GenericRecord';
 import { mapActions, mapMutations } from 'vuex'
 
 export default {
-    name: "NewRecordDialog",
+  name: 'NewRecordDialog',
 
-    components: {
-        ItAppForm,
-        GenericRecord
-    },
+  components: {
+    ItAppForm,
+    GenericRecord
+  },
 
-    methods: {
-        ...mapActions('AppBuilder', {
-            getAppList: 'getAppList'
-        }),
+  data: () => ({
+    tab: 0,
+    items: [
+      {
+        tab: 'ItApp',
+        component: ItAppForm,
+        id: 0
+      }
+    ]
+  }),
 
-        ...mapMutations('RecordsInstance', {
-            setCurrentRecord: 'setCurrentRecord',
-            setDisplayShow: 'setDisplayAppBuilderShow'
-        }),
-
-        closeModal(){
-            this.$emit('closeModal');
-        },
-        closingGenericRecord() {
-            this.closeModal()
-        }
-    },
-
-    data: () => ({
-        tab: null,
-        items:[
-            {
-                tab: "ItApp",
-                component: ItAppForm
-            }
-        ]
-    }),
- 
-    async mounted() {
-        try {
-            const res = await this.getAppList()
-            for (let x = 0; x < res.length; x++) {
-               this.items.push({ isGeneric: true, tab: res[x].title, component: "GenericRecord", app: res[x] }) 
-            }
-        } catch(e) {
-        }
+  async mounted() {
+    try {
+      const res = await this.getAppList()
+      for (let x = 0; x < res.length; x++) {
+        this.items.push({
+          isGeneric: true,
+          tab: res[x].title,
+          component: 'GenericRecord',
+          app: res[x],
+          id: x + 1
+        })
+      }
+    } catch (e) {
     }
+  },
+
+  methods: {
+    ...mapActions('AppBuilder', {
+      getAppList: 'getAppList'
+    }),
+
+    ...mapMutations('RecordsInstance', {
+      setCurrentRecord: 'setCurrentRecord',
+      setDisplayShow: 'setDisplayAppBuilderShow'
+    }),
+
+    closeModal() {
+      this.$emit('closeModal')
+    },
+    closingGenericRecord() {
+      this.closeModal()
+    }
+  }
 }
 </script>
+
 <style scoped>
 .record-text {
     height: 80vh;
