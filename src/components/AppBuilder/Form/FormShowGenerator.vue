bc99d6f11bb7076afccf4af5a80221e289646eba
cristian <segoviacristian23@hotmail.com>
2020-12-16 19:39:23 -0300

    M6PLAT-71 - work in progress

--- src/components/AppBuilder/Form/FormShowGenerator.vue
+++ src/components/AppBuilder/Form/FormShowGenerator.vue
@@ -22,43 +22,65 @@
           v-for="f in fields"
           :key="`custom-field-${f.id}`"
           cols="12"
+          :class=" f.type == 'referenced' ? 'custom-flex-row' : '' "
         >
-          <template v-if="f.machine_name === 'rapid_snapshot_image'">
-            <img
-              alt="Rapid Image"
-              :src="genericRecord[`${f.id}`]"
-              style="width: 20rem; height: auto;"
-            >
+          <template v-if="f.type == 'referenced' " >
+            <v-autocomplete 
+              class="mr-2 w-20" 
+              :items="recordsByAppsList[$h.dg( f, 'metadata.originalReference.id', '')]" 
+              item-text="title" 
+              item-value="id"
+              :value="f.referenced_record_id"
+              return-object
+              outlined 
+              filled 
+              label="Pick a Record" 
+              @input=" e => changingRefValue(e, f) "
+            />
           </template>
-          <component
-            :is=" $h.dg( typeToComponentMapping[f.metadata.originalReference.type], 'component', '')"
-            v-else-if="f.type === 'referenced'"
-            v-model="genericRecord[`${f.id}`]"
-            :chips="$h.dg(typeToComponentMapping[f.metadata.originalReference.type], 'chips', false)"
-            :clearable="$h.dg( typeToComponentMapping[f.metadata.originalReference.type], 'clearable', false )"
-            disabled
-            filled
-            :items="$h.dg( f, 'metadata.options', [] )"
-            :label=" $h.dg( f, 'label', '' ) "
-            :multiple="$h.dg(typeToComponentMapping[f.metadata.originalReference.type], 'multiple', false)"
-            outlined
-            :rules=" $h.dg( f, 'metadata.required', false) ? formRules.standard : []"
-            :type=" $h.dg( typeToComponentMapping[f.metadata.originalReference.type], 'type', '' ) "
-          />
-          <component
-            :is=" $h.dg( typeToComponentMapping[f.type], 'component', '')"
-            v-else
-            v-model="genericRecord[`${f.id}`]"
-            :chips="$h.dg(typeToComponentMapping[f.type], 'chips', false)"
-            :clearable="$h.dg( typeToComponentMapping[f.type], 'clearable', false )"
-            filled
-            :items="$h.dg( f, 'metadata.options', [] )"
-            :label=" $h.dg( f, 'label', '' ) "
-            :multiple="$h.dg(typeToComponentMapping[f.type], 'multiple', false)"
-            outlined
-            :rules=" $h.dg( f, 'metadata.required', false) ? formRules.standard : []"
-            :type=" $h.dg( typeToComponentMapping[f.type], 'type', '' ) "
-          />
+
+          <template v-else-if="f.type == 'referencedToApp'" >
+            App Name: {{ $h.dg(f, 'metadata.originalReference.label', '') }}
+          </template>
+
+          <template v-if="f.type != 'referencedToApp'" >
+
+            <template v-if="f.machine_name === 'rapid_snapshot_image'">
+              <img
+                alt="Rapid Image"
+                :src="genericRecord[`${f.id}`]"
+                style="width: 20rem; height: auto;"
+              >
+            </template>
+            <component
+              :is=" $h.dg( typeToComponentMapping[f.metadata.originalReference.type], 'component', '')"
+              v-else-if="f.type === 'referenced'"
+              v-model="genericRecord[`${f.id}`]"
+              :chips="$h.dg(typeToComponentMapping[f.metadata.originalReference.type], 'chips', false)"
+              :clearable="$h.dg( typeToComponentMapping[f.metadata.originalReference.type], 'clearable', false )"
+              filled
+              :items="$h.dg( f, 'metadata.options', [] )"
+              :label=" $h.dg( f, 'label', '' ) "
+              :multiple="$h.dg(typeToComponentMapping[f.metadata.originalReference.type], 'multiple', false)"
+              outlined
+              :rules=" $h.dg( f, 'metadata.required', false) ? formRules.standard : []"
+              :type=" $h.dg( typeToComponentMapping[f.metadata.originalReference.type], 'type', '' ) "
+            />
+            <component
+              :is=" $h.dg( typeToComponentMapping[f.type], 'component', '')"
+              v-else
+              v-model="genericRecord[`${f.id}`]"
+              :chips="$h.dg(typeToComponentMapping[f.type], 'chips', false)"
+              :clearable="$h.dg( typeToComponentMapping[f.type], 'clearable', false )"
+              filled
+              :items="$h.dg( f, 'metadata.options', [] )"
+              :label=" $h.dg( f, 'label', '' ) "
+              :multiple="$h.dg(typeToComponentMapping[f.type], 'multiple', false)"
+              outlined
+              :rules=" $h.dg( f, 'metadata.required', false) ? formRules.standard : []"
+              :type=" $h.dg( typeToComponentMapping[f.type], 'type', '' ) "
+            />
+          </template>
         </v-col>
       </v-row>
 
@@ -165,7 +187,8 @@ export default {
     typesToIds: {},
     complexDataStructs: { autocomplete: true, people: true },
     recordToEdit: {},
-    statusOptions: ['Published', 'Draft', 'Archived']
+    statusOptions: ['Published', 'Draft', 'Archived'],
+    recordsByAppsList: {}
   }),
 
   computed: {
@@ -181,7 +204,9 @@ export default {
       bulkSaveFieldValues: 'bulkSaveFieldValues',
       getFieldValuesPerPanel: 'getFieldValuesPerPanel',
       updateSomeFieldValues: 'updateSomeFieldValues',
-      deleteFieldsByIds: 'deleteFieldsByIds'
+      deleteFieldsByIds: 'deleteFieldsByIds',
+      recordsByApps: 'recordsByApps',
+      getSingleRecordFieldValue: 'getSingleRecordFieldValue'
     }),
 
     ...mapMutations('SnackBarNotif', {
@@ -263,18 +288,10 @@ export default {
           createObj[a] = toCreate
         })
 
-        const payload = {
-          record_id: this.$route.params.id,
-          fields: []
-        }
+        const payload = this.createFieldsPayload(newGenericRecord)
 
-        payload.fields = this.createFieldsPayload(newGenericRecord)
+        const payloadToCreate = this.createFieldsPayload(createObj)
 
-        const payloadToCreate = {
-          record_id: this.$route.params.id,
-          fields: []
-        }
-        payloadToCreate.fields = this.createFieldsPayload(createObj)
         await this.updateSomeFieldValues(payload)
         await this.bulkSaveFieldValues(payloadToCreate)
         if (deleteArr.length) {
@@ -294,9 +311,10 @@ export default {
 
     createFieldsPayload(record) {
       let fields = []
-
       for (let x = 0; x < this.fields.length; x++) {
         const f = this.fields[x]
+        const idForUpdate = this.$h.dg(f, 'metadata.originalReference.id', '') ? f.metadata.originalReference.id  : f.id 
+        const recordIdForUpdate = this.$h.dg( f, 'referenced_record_id', '') ? f.referenced_record_id : this.$route.params.id
         let value = this.$h.dg(record, `${f.id}`, '')
 
         if (!value) continue
@@ -304,21 +322,22 @@ export default {
         if (Array.isArray(value)) {
           const res = value.map(v => ({
             value: v,
-            field_id: f.id
+            field_id: idForUpdate,
+            record_id: recordIdForUpdate
           }))
           fields = [...fields, ...res]
         }
         if (Object.prototype.toString.call(value) == '[object Object]') {
           delete value['created_at']
           delete value['updated_at']
-          fields.push({ value, field_id: f.id })
+          fields.push({ value, field_id: idForUpdate, record_id: recordIdForUpdate })
         } else {
           if (value == 'true' || value == 'false') value = value == 'true'
-          fields.push({ value, field_id: f.id })
+          fields.push({ value, field_id: idForUpdate, record_id: recordIdForUpdate })
         }
       }
 
-      return fields
+      return { fields }
     },
 
     findTheDifference(reference, newData, fieldId) {
@@ -348,6 +367,29 @@ export default {
           this.loading = false
         }
       }
+    },
+
+    async changingRefValue(record, field) {
+      const fieldID = field.metadata.originalReference.id
+      try {
+        const res = await this.getSingleRecordFieldValue({ recordID: record.id, fieldID, refID: field.id})
+        const genericRecord = { ...this.genericRecord }
+
+        switch (true) {
+          case this.$h.dg(field, 'metadata.originalReference.type') == 'timestamp':
+            genericRecord[`${field.id}`] = this.$h.dg(res, '0.value', '')
+            break;
+        
+          default:
+            genericRecord[`${field.id}`] = res.value
+            break;
+        }
+
+        this.genericRecord = {...genericRecord}
+        field.referenced_record_id = record.id
+      } catch(e) {
+        this.notifDanger('There was an error while getting a reference fields value')
+      }
     }
 
   },
@@ -358,10 +400,39 @@ export default {
     }
   },
 
-  mounted() {
-    this.loadingData()
-    this.recordToEdit = { ...this.currentRecord }
+  async mounted() {
+    try {
+      this.loadingData()
+      this.recordToEdit = { ...this.currentRecord }
+      const referencedFields = this.fields.filter( f => f.type == 'referenced' )
+
+      if(referencedFields.length) {
+        const originals = referencedFields
+          .map( r => {
+            return { 
+              app_id: this.$h.dg(r, 'metadata.originalReference.app_id' ,''), 
+              id:  this.$h.dg(r, 'metadata.originalReference.id' ,''),
+              type: this.$h.dg(r, 'metadata.originalReference.type', '') 
+            }
+          })
+        
+        this.recordsByAppsList = await this.recordsByApps({ fields: originals })
+      }
+    } catch(e) {
+      this.notifDanger('There was an error while loading')
+    }
   }
 
 }
 </script>
+
+<style lang="scss" scoped>
+.custom-flex-row {
+  display: flex; 
+  display: -webkit-flex;
+  flex-direction: row;
+}
+.w-20 {
+  width: 20% !important;
+}
+</style>
