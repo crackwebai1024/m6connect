61948135260b5b767a49b78c48d42f69b6671bff
jacob-liu-g <spring.scene@hotmail.com>
2020-12-27 17:09:50 +0000

    Implement App Field movement inside panel

--- src/components/AppBuilder/Panel.vue
+++ src/components/AppBuilder/Panel.vue
@@ -76,47 +76,55 @@
     </template>
 
     <v-list>
-      <v-list-item
-        v-for="field in panel.fields"
-        :key="field.id"
+      <draggable
+        v-model="panel.fields"
+        @end="drag=true"
+        @start="drag=true"
+        @change="onFieldMove"
       >
-        <v-list-item-content @click="editField(field)">
-          <v-list-item-title :class="(field.type === 'referenced') || field.type === 'referencedToApp' ? 'referenced-field' : ''">
-            {{ field.label }}
-          </v-list-item-title>
-          <v-list-item-subtitle
-            v-if="field.type === 'referenced'"
-            class="font-italic"
-          >
-            Referenced Field {{ field.metadata.originalReference.label }}
-          </v-list-item-subtitle>
-          <v-list-item-subtitle
-            v-if="field.type === 'referencedToApp'"
-            class="font-italic"
-          >
-            Referenced App {{ field.metadata.originalReference.label }}
-          </v-list-item-subtitle>
-        </v-list-item-content>
-        <v-list-item-action>
-          <v-btn
-            icon
-            @click="showDelete(field)"
-          >
-            <v-icon color="red lighten-3">
-              mdi-delete
-            </v-icon>
-          </v-btn>
-        </v-list-item-action>
-      </v-list-item>
+        <v-list-item
+          v-for="field in panel.fields"
+          :key="field.id"
+        >
+          <v-list-item-content @click="editField(field)">
+            <v-list-item-title :class="(field.type === 'referenced') || field.type === 'referencedToApp' ? 'referenced-field' : ''">
+              {{ field.label }}
+            </v-list-item-title>
+            <v-list-item-subtitle
+              v-if="field.type === 'referenced'"
+              class="font-italic"
+            >
+              Referenced Field {{ field.metadata.originalReference.label }}
+            </v-list-item-subtitle>
+            <v-list-item-subtitle
+              v-if="field.type === 'referencedToApp'"
+              class="font-italic"
+            >
+              Referenced App {{ field.metadata.originalReference.label }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-btn
+              icon
+              @click="showDelete(field)"
+            >
+              <v-icon color="red lighten-3">
+                mdi-delete
+              </v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </draggable>
       <v-list-item v-if="showTableCreator" >
         <table-creator :table="activeAppTable" @updateTable="updatingTable" @close="showTableCreator = false" />
       </v-list-item>
       <template v-else >
         <v-list-item v-for="(table, index) in panel.tables" @click="editingTable(table)" :key="`panel-table-${index}`" >
-          {{ table.title }}   
-        </v-list-item> 
+          {{ table.title }}
+        </v-list-item>
       </template>
     </v-list>
+
     <div class="align-start d-flex">
       <div class="overflow-hidden w-full">
         <add-field @addNewField="addNewField" />
@@ -160,6 +168,7 @@ import Field from '@/components/AppBuilder/Modals/Field'
 import DeleteDialog from '@/components/Dialogs/DeleteDialog'
 import TableCreator from '@/components/AppBuilder/GenericTable'
 import { mapActions, mapMutations } from 'vuex'
+import Draggable from 'vuedraggable'
 
 export default {
   name: 'Panel',
@@ -168,7 +177,8 @@ export default {
     DeleteDialog,
     AddField,
     Field,
-    TableCreator
+    TableCreator,
+    Draggable
   },
 
   props: {
@@ -214,7 +224,8 @@ export default {
 
   methods: {
     ...mapActions('AppBuilder', {
-      updatePanel: 'updatePanel'
+      updatePanel: 'updatePanel',
+      moveField: 'moveField'
     }),
 
     ...mapActions('AppTables', {
@@ -226,7 +237,7 @@ export default {
       notifSuccess: 'notifSuccess'
     }),
 
-    updatingTable(table) { 
+    updatingTable(table) {
       this.activeAppTable = {...table}
       this.$emit('updatingTable', table)
     },
@@ -234,7 +245,6 @@ export default {
     editingTable(table){
       this.activeAppTable = table
       if( !this.$h.dg(this.activeAppTable, 'fields', []).length ) this.activeAppTable.fields = []
-
       this.$nextTick( () => { this.showTableCreator = true })
     },
 
@@ -318,6 +328,30 @@ export default {
       }
       this.showFieldModal = false
       this.editing = false
+    },
+
+    async onFieldMove(evt) {
+      if (!evt.moved) {
+        return;
+      }
+
+      const ctx = evt.moved;
+      this.loading = true
+
+      // Dispatch move action
+      try {
+        await this.moveField({
+          id: ctx.element.id,
+          newWeight: ctx.newIndex
+        })
+
+        this.notifSuccess('Movement saved')
+      } catch (e) {
+        this.notifDanger('There was an error while saving move')
+        return false
+      } finally {
+        this.loading = false
+      }
     }
   }
 }
